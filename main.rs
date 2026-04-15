b01f2047033a0a5b57d4d1197bb141ee8b22cbbd
Miracle Okolo <83901702+miracle101000@users.noreply.github.com>
2026-04-15 20:27:44 +0000

    fixing bugs

--- main.rs
+++ main.rs
@@ -1,67 +1,142 @@
 mod models;
-
-use axum::Error;
-use axum::serve::Listener;
-use axum::{routing::{post, get, delete}, Json, Router, extract::State};
+use axum::{ Json, Router, extract::{State, ws::{Message, WebSocket, WebSocketUpgrade}}, routing::{ delete, get, patch, post } };
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
+use axum_valid::Valid;
+use chrono::Utc;
+use tokio::sync::broadcast;
 use validator::Validate;
-use std::sync::{Mutex, Arc};
-use models::{TodoItem,TodoItemStatus};
+use parking_lot::Mutex;
+use std::sync::{ Arc };
+use models::{ TodoItem, TodoItemStatus };
 
-type SharedState = Arc<Mutex<Vec<TodoItem>>>;
+use crate::models::{ AppState, CreateTodoRequest, IdPath };
 
-async fn add_todo_item(State(todo_list): State<SharedState>,
-  Json(payload): Json<TodoItem>)-> impl IntoResponse{
+async fn handler(ws:WebSocketUpgrade,State(state): State<AppState>) -> impl IntoResponse {
+    ws.on_upgrade(move |socket| handle_websocket(socket,state))
+}
 
-   if let Err(errors) = payload.validate() {
-     return  (StatusCode::BAD_REQUEST, format!("Invalid input {}", errors)).into_response();
-   }
+async fn handle_websocket(mut socket: WebSocket, state: AppState) {
+    let mut rx = state.tx.subscribe();
+    while let Ok(item) = rx.recv().await {
+       
+       match serde_json::to_string(&item){
+         Ok(json) => {
+           if socket.send(Message::Text(json.into())).await.is_err(){
+                break;
+            } 
+         }
+         Err(e)=>{
+            eprint!("Failed to serialize item {}",e);
+         }
 
-    let mut list = todo_list.lock().unwrap();
-
-    list.push(payload.clone());
-
-    Json(payload).into_response()
+       }
+            
   }
+}
 
-async fn get_todo_items(State(todo_list):State<SharedState>)->Json<Vec<TodoItem>>{
-    let  list = todo_list.lock().unwrap();
+async fn add_todo_item(
+    State(state): State<AppState>,
+    Json(payload): Json<CreateTodoRequest>
+) -> impl IntoResponse {
+    if let Err(errors) = payload.validate() {
+        return (StatusCode::BAD_REQUEST, format!("Invalid input {}", errors)).into_response();
+    }
+
+    let mut list = state.todo_list.lock();
+
+    let initial_timestamp = Utc::now();
+
+    let new_id = IdPath {
+        id: uuid::Uuid::new_v4(),
+    };
+
+    let new_item = TodoItem {
+        id: new_id,
+        title: payload.title,
+        content: payload.content,
+        status: TodoItemStatus::Undone,
+        created_at: initial_timestamp,
+        updated_at: initial_timestamp,
+    };
+
+    list.push(new_item.clone());
+
+    let _ = state.tx.send(new_item.clone());
+
+    Json(new_item).into_response()
+}
+
+async fn get_todo_items(State(state): State<AppState>) -> Json<Vec<TodoItem>> {
+    let list = state.todo_list.lock();
     Json(list.to_vec())
 }
 
-async fn update_todo_item_status (){}
+async fn update_todo_item_status_to_done(
+    State(state): State<AppState>,
+    Valid(Json(payload)): Valid<Json<IdPath>>
+) -> impl IntoResponse {
+    let mut list = state.todo_list.lock();
+    let index = list.iter().position(|item| item.id.id.to_string() == payload.id.to_string());
 
-async fn delete_todo_item(State(todo_list): State<SharedState>, Json(id): Json<String>) -> impl  IntoResponse{
- let mut list =  todo_list.lock().unwrap();
- let index  = list.iter().position(|item| item.id == id);
+    match index {
+        Some(index) => {
+            let updated_item = list[index].copy_with(None, None, Some(TodoItemStatus::Done));
 
- match index {
-    Some(index) => {
-        list.remove(index);
-        StatusCode::OK.into_response()
+            list[index] = updated_item.clone();
+
+            Json(updated_item).into_response()
+        }
+        None => {
+            (
+                StatusCode::NOT_FOUND,
+                format!("Item with id: {} not found", payload.id),
+            ).into_response()
+        }
     }
-    None => {
-        (StatusCode::NOT_FOUND, format!("Item with id: {} not found",id)).into_response()
-    }
- }
-
 }
 
+async fn delete_todo_item(
+    State(state): State<AppState>,
+    Valid(Json(payload)): Valid<Json<IdPath>>
+) -> impl IntoResponse {
+    let mut list = state.todo_list.lock();
+    let index = list.iter().position(|item| item.id.id.to_string() == payload.id.to_string());
+
+    match index {
+        Some(index) => {
+            list.remove(index);
+            StatusCode::OK.into_response()
+        }
+        None => {
+            (
+                StatusCode::NOT_FOUND,
+                format!("Item with id: {} not found", payload.id),
+            ).into_response()
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let todo_list = Arc::new(Mutex::new(Vec::<TodoItem>::new()));
 
+    let (tx, _) = broadcast::channel::<TodoItem>(100);
+    
+    let state =  AppState {
+        todo_list,
+        tx
+    };
+
     let app = Router::new()
-    .route("/add_todo_item", post(add_todo_item))
-    .route("/get_todo_items", get(get_todo_items))
-    .route("/delete_todo_item", delete(delete_todo_item))
-    .with_state(todo_list);
+    .route("/ws", get(handler))
+        .route("/add_todo_item", post(add_todo_item))
+        .route("/get_todo_items", get(get_todo_items))
+        .route("/update_todo_item_to_done", patch(update_todo_item_status_to_done))
+        .route("/delete_todo_item", delete(delete_todo_item))
+        .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
-    axum::serve(listener,app).await.unwrap();
-
-
+    axum::serve(listener, app).await.unwrap();
 }
--- models.rs
+++ models.rs
@@ -1,18 +1,66 @@
-use chrono::{DateTime, Utc};
-use serde::{Serialize, Deserialize};
-use validator::{Validate};
+use std::sync::Arc;
+
+use chrono::{ DateTime, Utc };
+use parking_lot::Mutex;
+use serde::{ Serialize, Deserialize };
+use validator::{ Validate };
+use tokio::sync::broadcast::{Sender};
 
 #[derive(Debug, Default, Serialize, Deserialize, Clone)]
-pub enum TodoItemStatus { #[default] Undone, Done}
+pub enum TodoItemStatus {
+    #[default] Undone,
+    Done,
+}
 
 #[derive(Serialize, Deserialize, Debug, Clone, Validate)]
 pub struct TodoItem {
-   pub id: String,
+    #[serde(flatten)]
+    pub id: IdPath,
     #[validate(length(min = 1, message = "Length must be greater than 1"))]
-   pub title: String,
+    pub title: String,
     #[validate(length(min = 1, max = 500))]
-   pub content: String,
-   pub status: TodoItemStatus,
-    created_at: DateTime<Utc>,
-    update_at:  DateTime<Utc>
+    pub content: String,
+    pub status: TodoItemStatus,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
 }
+
+impl TodoItem {
+    pub fn copy_with(
+        &self,
+        title: Option<String>,
+        content: Option<String>,
+        status: Option<TodoItemStatus>
+    ) -> Self {
+        Self {
+            id: self.id.clone(),
+            title: title.unwrap_or_else(|| self.title.clone()),
+            content: content.unwrap_or_else(|| self.content.clone()),
+            status: status.unwrap_or_else(|| self.status.clone()),
+            created_at: self.created_at,
+            updated_at: Utc::now(),
+        }
+    }
+}
+
+#[derive(Deserialize, Validate)]
+pub struct CreateTodoRequest {
+    #[validate(length(min = 1, message = "Length must be greater than 1"))]
+    pub title: String,
+    #[validate(length(min = 1, max = 500))]
+    pub content: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Validate)]
+pub struct IdPath {
+    pub id: uuid::Uuid,
+}
+
+
+#[derive(Clone)]
+pub struct AppState {
+    pub todo_list: Arc<Mutex<Vec<TodoItem>>>,
+    pub tx: Sender<TodoItem>
+}
+
+
